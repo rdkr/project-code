55fbde9d942b9b59f37166907e9329b10a783918
Gov <govind.suresh1@gmail.com>
2019-05-20 23:44:28 +0100

    Corrected comments

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return("<h1>Home Page<h1>"+"<h6>Hello, world!<h6>")
-#If run from docker specify the host and no debug mode
+
 if __name__ == '__main__':
     app.run()
